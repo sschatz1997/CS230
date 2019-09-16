1958f7cfa78a02c34ba913d6fef837f5a4cb810e
sschatz1997 <s.schazt1997@gmail.com>
2019-09-16 13:07:44 -0400

    got some work done, Sam keep up the good work!

---
+++ Lab_1/Info.h
@@ -0,0 +1,31 @@
+//Info.h
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stdlib.H>
+#include <fstream>
+
+
+using namespace std;
+
+class Info
+{
+	public:
+		Info();
+
+		string getStu_ID();
+		string getF_name();
+		string getL_name();
+		string getClas_ID();
+		string getCourse();
+		string get
+
+
+
+	private:
+
+
+
+
+}
---
+++ Lab_1/main.cpp
@@ -0,0 +1,20 @@
+//main.cpp
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <stdlib.H>
+#include <fstream>
+
+using namespace std;
+
+int main(int arg, char** argv)
+{
+
+
+
+
+
+
+	return 0;
+}
