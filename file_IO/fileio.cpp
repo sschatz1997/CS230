05dcdf78282dfefe4603f5eb01f7e13c343ab258
sschatz1997 <s.schazt1997@gmail.com>
2019-09-19 11:09:38 -0400

    fixes

--- file_IO/fileio.cpp
+++ file_IO/fileio.cpp
@@ -5,23 +5,60 @@
 
 using namespace std;
 
-void writeToFile()
+void readFromFile()
 {
-
+	string line;
+	ifstream inData ("namesFile.txt");
+	if (inData.is_open())
+	{
+		while (getline (inData,line))
+		{
+			cout << line << "\n";
+		}
+		inData.close();
+	}
+	else
+		cout << "File doesnt exist\n";
 
 }
 
 
-int  writeToBuffer(string* namePTR, int cout)
+void writeToFile(string* namePTR, int count)
 {
-	string tempName;
+	ofstream outData;
+	outData.open("namesFile.txt", ios::app);
+	for (int i = 0; i < count; i++)
+	{
+		outData << *namePTR << endl;
+		namePTR++;
+	}
+	outData.close();
+
+}
+
+
+int  writeToBuffer(string* namePTR, int count)
+{
+	string name;
 	for (int i = 0; i < count; i++)
 	{
 		namePTR++;
 	}
 	cout << "Enter last name: ";
-	cin >> tempName;
+	cin >> name;
+	*namePTR = name;
+	namePTR++;
+	count++;
+	return count;
+}
 
+void printAll(string* namePTR, int count)
+{
+	for(int i = 0; i < count; i++)
+	{
+		cout << "name[" << i << "]: " << *namePTR << endl;
+		namePTR++;
+	}
 }
 
 
@@ -30,6 +67,9 @@ int main()
 	int count = 0;
 	int choice;
 	string names[100];
+	
+	readFromFile();
+	
 	do
 	{
 		cout << "enter record 1 for yes, 2 for no: ";
@@ -40,7 +80,11 @@ int main()
 		}
 	}while(choice != 2);
 
-	count = writeToFile(names, count);
+	//count = writeToFile(names, count);
+	cout << " data in buffer is: \n";
+	
+	printAll(names, count);
+	writeToFile(names, count);
 
 	return 0;
 }
